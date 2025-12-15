422a3aa9470e4262a9f79e17aa5c426ef515b891
sarish339 <sarishsunkara339@gmail.com>
2025-12-15 10:42:02 +0530

    add code

---
+++ variables/variables.tf
@@ -0,0 +1,53 @@
+# 1. Command Line
+# 2. Terraform.tfvars
+# 3. Environmnetal Variable, TF_VAR_variable_name
+# 4. Default
+# 5. Prompt
+variable ami_id {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "This is the AMI ID of devops-practice of RHEL 9"
+}
+
+variable "instance_type" {
+    type = string
+    #default = "t3.micro"
+}
+
+variable "tags"{
+    type = map #optional
+    default = {
+        Name = "backend"
+        Project = "expense"
+        Component = "backend"
+        Environment = "DEV"
+        Terraform = "true"
+    }
+}
+
+variable "sg_name"{
+    default = "allow_sshh"
+}
+
+variable "sg_description"{
+    default ="Allow port number 22 for SSH access"
+}
+
+variable "from_port"{
+    default = 22
+    type = number
+}
+
+variable "to_port"{
+    default = 22
+    type = number
+}
+
+variable "protocol"{
+    default = "tcp"
+}
+
+variable "ingress_cidr"{
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
